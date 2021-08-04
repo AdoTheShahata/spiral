ee5299673e4c397fc7038f9d2a624aa8848748b4
AdoTheShahata <80445192+AdoTheShahata@users.noreply.github.com>
2021-08-04 18:32:08 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,44 @@
+let player = game.createSprite(0, 0)
+for (let index = 0; index < 4; index++) {
+    player.move(1)
+    basic.pause(100)
+}
+player.turn(Direction.Right, 90)
+for (let index = 0; index < 4; index++) {
+    player.move(1)
+    basic.pause(100)
+}
+player.turn(Direction.Right, 90)
+for (let index = 0; index < 4; index++) {
+    player.move(1)
+    basic.pause(100)
+}
+player.turn(Direction.Right, 90)
+for (let index = 0; index < 3; index++) {
+    player.move(1)
+    basic.pause(100)
+}
+player.turn(Direction.Right, 90)
+for (let index = 0; index < 3; index++) {
+    player.move(1)
+    basic.pause(100)
+}
+player.turn(Direction.Right, 90)
+for (let index = 0; index < 2; index++) {
+    player.move(1)
+    basic.pause(100)
+}
+player.turn(Direction.Right, 90)
+for (let index = 0; index < 2; index++) {
+    player.move(1)
+    basic.pause(100)
+}
+player.turn(Direction.Right, 90)
+basic.pause(100)
+player.move(1)
+player.turn(Direction.Right, 90)
+basic.pause(100)
+player.move(1)
+basic.forever(function () {
+	
+})
